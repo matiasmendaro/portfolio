bd4cffdcc343dadd75648496a8e2e97f2fb5b91a
matias <mendaro.matias@gmail.com>
2024-11-30 20:48:28 -0300

    first commit portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,10 @@ import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import mdx from '@astrojs/mdx';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
-  integrations: [tailwind(), icon(), mdx()]
+  integrations: [tailwind(), icon(), mdx()],
+  output: 'server',
+  adapter: netlify()
 });
